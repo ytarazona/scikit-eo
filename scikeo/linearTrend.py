d31f0686c9e7bea0e98058fd5040aef9c51b3594
Yonatan Tarazona Coronel <geoyons@gmail.com>
2022-11-03 16:15:24 -0500

    updating "linearTrend"

--- scikeo/linearTrend.py
+++ scikeo/linearTrend.py
@@ -37,25 +37,6 @@ class linearTrend(object):
             a dictionary with slope, intercept and p-value obtained. All of them in numpy.ndarray 
             with 2d.
     
-    
-        References:
-        - Crist, E.P., R. Laurin, and R.C. Cicone. 1986. Vegetation and soils information 
-          contained in transformed Thematic Mapper data. Pages 1465-1470 Ref. ESA SP-254. 
-          European Space Agency, Paris, France. http://www.ciesin.org/docs/005-419/005-419.html.
-    
-        - Baig, M.H.A., Shuai, T., Tong, Q., 2014. Derivation of a tasseled cap transformation 
-          based on Landsat 8 at-satellite reflectance. Remote Sensing Letters, 5(5), 423-431. 
-    
-        - Li, B., Ti, C., Zhao, Y., Yan, X., 2016. Estimating Soil Moisture with Landsat Data 
-          and Its Application in Extracting the Spatial Distribution of Winter Flooded Paddies. 
-          Remote Sensing, 8(1), 38.
-    
-        Note:
-        Linear regression is widely used to analyze forest degradation or land degradation.
-        Specifically, the slope and its reliability are used as main parameters and they
-        can be obtained with this function. On the other hand, logistic regression allows
-        obtaining a degradation risk map, in other words, it is a probability map.
-        
         References:
         - Tarazona, Y., Maria, Miyasiro-Lopez. (2020). Monitoring tropical forest degradation using
           remote sensing. Challenges and opportunities in the Madre de Dios region, Peru. Remote
@@ -65,6 +46,12 @@ class linearTrend(object):
           analysis of variance. Appl. Stat. 22, 392-399.
         
         - Chambers, J.M., 1992. Statistical Models in S. CRS Press.
+    
+        Note:
+        Linear regression is widely used to analyze forest degradation or land degradation.
+        Specifically, the slope and its reliability are used as main parameters and they
+        can be obtained with this function. On the other hand, logistic regression allows
+        obtaining a degradation risk map, in other words, it is a probability map.
         '''
         
         if not isinstance(self.image, (rasterio.io.DatasetReader)):
