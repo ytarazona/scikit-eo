f6ebbf27a4678b17c46efb3b48533775c8a424c1
Yonatan Tarazona Coronel <geoyons@gmail.com>
2022-11-05 20:06:36 -0500

    version package python

--- setup.py
+++ setup.py
@@ -52,6 +52,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/ytarazona/scikit-eo',
-    version='0.1.9',
+    version='0.1.91',
     zip_safe=False,
 )
